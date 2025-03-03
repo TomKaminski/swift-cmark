8b164d05983384f9c5cfe6f622012bebd8b6f2ea
Tomasz Kaminski <tkaminski93@gmail.com>
2025-03-03 09:45:12 +0100

    left && right flanking

--- extensions/subscript.c
+++ extensions/subscript.c
@@ -32,7 +32,7 @@ static cmark_node *match(cmark_syntax_extension *self, cmark_parser *parser,
   res->start_line = res->end_line = cmark_inline_parser_get_line(inline_parser);
   res->start_column = cmark_inline_parser_get_column(inline_parser) - delims;
 
-  if(left_flanking && right_flanking) {
+  if (left_flanking || right_flanking) {
     cmark_inline_parser_push_delimiter(inline_parser, character, left_flanking, right_flanking, res);
   }
 
